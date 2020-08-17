2b2257bb21b10332d3b0b62a161adbedca6edbdc
Maxim Ivanov <ivanovmg@gmail.com>
2020-08-18 00:38:38 +0700

    Add cli test for --keep-comments

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -64,6 +64,44 @@ def test_command_line_interface(tmpdir):
     assert help_result.exit_code == 0
 
 
+def test_keep_comments(tmpdir):
+    """Test CLI when keep comments."""
+    input_file = tmpdir / 'input.tex'
+    input_content = dedent(
+        """\
+        Dude does mind!
+        % @dude, let's go bowling
+        \\textbf{  this aggression will not stand },
+        And this % also.
+        """
+    )
+    input_file.write(input_content)
+
+    expected_content = dedent(
+        """\
+        Dude does mind!
+        % @dude, let's go bowling
+        \\textbf{this aggression will not stand},
+        And this % also.
+        """
+    )
+
+    output_file = tmpdir / 'output.tex'
+
+    runner = CliRunner()
+    result = runner.invoke(
+        cli.main,
+        [
+            str(input_file),
+            str(output_file),
+            '--keep-comments',
+        ])
+    assert result.exit_code == 0
+
+    with open(output_file, 'r') as out:
+        assert expected_content == out.read()
+
+
 def test_non_tex_file_aborts(tmpdir):
     """Check that click.ClickException is raised for non-tex file."""
     input_file = tmpdir / 'input.dat'
